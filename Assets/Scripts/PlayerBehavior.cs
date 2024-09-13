9d32f976a022284ba88adf5f162c8f9a3954edb5
Asier <149468225+mdc-ara@users.noreply.github.com>
2024-09-13 15:06:59 +0200

    Ch07 page 194
    Rigidbody methods MovePosition y MoveRotation

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // 1
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
 
-    // 2
     private float vInput;
     private float hInput;
+    
+    // 1
+    private Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 3
+        _rb = GetComponent<Rigidbody>();
+        Debug.Log(Time.fixedDeltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 3
         vInput = Input.GetAxis("Vertical") * moveSpeed;
-
-        // 4
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        // 5
+        /*
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-
-        // 6
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
-
+        */
+    }
+    //1
+    void FixedUpdate() {  
+        // 2
+        Vector3 rotation = Vector3.up * hInput;
+        // 3
+        Quaternion angleRot = Quaternion.Euler(rotation *  Time.fixedDeltaTime);
+        // 4
+        _rb.MovePosition(this.transform.position + this.transform.forward* vInput * Time.fixedDeltaTime);
+        // 5
+        _rb.MoveRotation(_rb.rotation* angleRot);
     }
 }
