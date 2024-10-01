e88a6834f802d607480121f5d2896df7b5eaf390
Asier <149468225+mdc-ara@users.noreply.github.com>
2024-10-01 18:23:52 +0200

    Ch08 page 214

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,19 +6,27 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+
+    // 1
+    public float distanceToGround = 0.1f;
+    // 2
+    public LayerMask groundLayer;
 
     private float vInput;
     private float hInput;
-    
-    // 1
     private Rigidbody _rb;
+
+    // 3
+    private CapsuleCollider _col;
 
     // Start is called before the first frame update
     void Start()
     {
         // 3
         _rb = GetComponent<Rigidbody>();
-        Debug.Log(Time.fixedDeltaTime);
+        // 4
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -33,14 +41,24 @@ public class PlayerBehavior : MonoBehaviour
         */
     }
     //1
-    void FixedUpdate() {  
+    void FixedUpdate() {
         // 2
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            // 3
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+
         Vector3 rotation = Vector3.up * hInput;
-        // 3
         Quaternion angleRot = Quaternion.Euler(rotation *  Time.fixedDeltaTime);
-        // 4
         _rb.MovePosition(this.transform.position + this.transform.forward* vInput * Time.fixedDeltaTime);
-        // 5
         _rb.MoveRotation(_rb.rotation* angleRot);
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
     }
 }
